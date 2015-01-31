e7358f9707555fa6c909ffe9637a41f44270ae77
Azu-Nya-n <tmg1epmt@yahoo.co.jp>
2015-01-31 17:58:15 +0900

    いろいろ

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -1,17 +1,27 @@
-#include<iostream>
+#include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
-int main()
+int main(void)//引数を取らない関数であることをvoidで宣言
 {
 
 	int a = 0;
+	int b = 0;
+	char mozi = 'A';
+
+	printf("int型の変数ｂに入力します　");
+	scanf_s("%d",&b);//scanf_sで変数に値を代入、scanfだとエラー
+
+	printf("moziは %c です\n", mozi);
+	printf("bは %d です\n", b);
 
 	while (a != 20)
 	{
+
 		cout << "任意の整数を入力してください please  "  ;
 		cin >> a;
-		cout << a << endl;
+		printf("入力された数値は %d です\n", a);
 	}
 
 	if (a == 20)
@@ -19,5 +29,5 @@ int main()
 		cout << "THE END ";
 	}
 
-	return 0;
+	return 0;//0を返すことによりプログラムを正常に終了させることを表す
 }
