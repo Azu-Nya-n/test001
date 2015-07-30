247ee41a7cd06c7269d7b8ed8be12c8940e37b4c
Azu-Nya-n <tmg1epmt@yahoo.co.jp>
2015-07-30 23:40:07 +0900

    test

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -10,11 +10,20 @@ int main(void)//引数を取らない関数であることをvoidで宣言
 	int b = 0;
 	char mozi = 'A';
 
-	printf("int型の変数ｂに入力します　");
+	printf("int型の変数ｂに\t入力します　");//\tでタブを挿入
 	scanf_s("%d",&b);//scanf_sで変数に値を代入、scanfだとエラー
 
 	printf("moziは %c です\n", mozi);
-	printf("bは %d です\n", b);
+
+	if (b == 10)
+	{
+		printf("bは10です");
+	}
+	else
+	{
+		printf("bは10ではなく %d です\n", b);
+	}
+	
 
 	while (a != 20)
 	{
